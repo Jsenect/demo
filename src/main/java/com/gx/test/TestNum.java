27c2f5be5d69cbac0f7e686cac56a0610ce88a3b
liuzhizheng <449217551@qq.com>
2020-04-15 09:46:31 +0800

    init

---
+++ src/main/java/com/gx/test/TestNum.java
@@ -0,0 +1,19 @@
+package com.gx.test;
+
+import java.util.List;
+
+/**
+ * @author zzl
+ * @date 2020/4/3 8:53
+ */
+public class TestNum {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
